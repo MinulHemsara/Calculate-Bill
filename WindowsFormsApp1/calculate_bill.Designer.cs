479bcb04d6df0da27f616c6c3ae87e2935ee5468
MinulHemsara <minulhemsara456@gmail.com>
2022-02-10 16:28:25 +0530

    Add project files.

---
+++ WindowsFormsApp1/calculate_bill.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class calculate_bill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cmb_type = new System.Windows.Forms.ComboBox();
+            this.txt_unit = new System.Windows.Forms.TextBox();
+            this.txt_tot = new System.Windows.Forms.TextBox();
+            this.btn_cal = new System.Windows.Forms.Button();
+            this.btn_cls = new System.Windows.Forms.Button();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Coustomer Type";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(81, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Units";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(80, 198);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Bill";
+            // 
+            // cmb_type
+            // 
+            this.cmb_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_type.FormattingEnabled = true;
+            this.cmb_type.Items.AddRange(new object[] {
+            "Domestic Customer",
+            "business customer"});
+            this.cmb_type.Location = new System.Drawing.Point(380, 56);
+            this.cmb_type.Name = "cmb_type";
+            this.cmb_type.Size = new System.Drawing.Size(183, 37);
+            this.cmb_type.TabIndex = 5;
+            // 
+            // txt_unit
+            // 
+            this.txt_unit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_unit.Location = new System.Drawing.Point(379, 137);
+            this.txt_unit.Name = "txt_unit";
+            this.txt_unit.Size = new System.Drawing.Size(153, 34);
+            this.txt_unit.TabIndex = 10;
+            // 
+            // txt_tot
+            // 
+            this.txt_tot.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_tot.Location = new System.Drawing.Point(379, 207);
+            this.txt_tot.Name = "txt_tot";
+            this.txt_tot.Size = new System.Drawing.Size(153, 43);
+            this.txt_tot.TabIndex = 11;
+            // 
+            // btn_cal
+            // 
+            this.btn_cal.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cal.Location = new System.Drawing.Point(149, 458);
+            this.btn_cal.Name = "btn_cal";
+            this.btn_cal.Size = new System.Drawing.Size(162, 55);
+            this.btn_cal.TabIndex = 12;
+            this.btn_cal.Text = "Calculate";
+            this.btn_cal.UseVisualStyleBackColor = true;
+            this.btn_cal.Click += new System.EventHandler(this.btn_cal_Click);
+            // 
+            // btn_cls
+            // 
+            this.btn_cls.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_cls.Location = new System.Drawing.Point(355, 458);
+            this.btn_cls.Name = "btn_cls";
+            this.btn_cls.Size = new System.Drawing.Size(154, 55);
+            this.btn_cls.TabIndex = 13;
+            this.btn_cls.Text = "Clear";
+            this.btn_cls.UseVisualStyleBackColor = true;
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_exit.Location = new System.Drawing.Point(541, 458);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(148, 55);
+            this.btn_exit.TabIndex = 14;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            // 
+            // calculate_bill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 642);
+            this.Controls.Add(this.btn_exit);
+            this.Controls.Add(this.btn_cls);
+            this.Controls.Add(this.btn_cal);
+            this.Controls.Add(this.txt_tot);
+            this.Controls.Add(this.txt_unit);
+            this.Controls.Add(this.cmb_type);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "calculate_bill";
+            this.Text = "Calculate Bill";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cmb_type;
+        private System.Windows.Forms.TextBox txt_unit;
+        private System.Windows.Forms.TextBox txt_tot;
+        private System.Windows.Forms.Button btn_cal;
+        private System.Windows.Forms.Button btn_cls;
+        private System.Windows.Forms.Button btn_exit;
+    }
+}
+
---
+++ WindowsFormsApp1/calculate_bill.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class calculate_bill : Form
+    {
+        public calculate_bill()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_cal_Click(object sender, EventArgs e)
+        {
+            double unit,eu,ec, tot;
+            unit =Convert.ToDouble(txt_unit.Text);
+            if(cmb_type.SelectedIndex == 0)
+            {
+                if (unit <= 100)
+                {
+                    tot = 500 + 200;
+                    txt_tot.Text = tot.ToString();
+                }
+                else
+                {
+                    eu = unit - 100;
+                    ec = eu * 20;
+                    tot = ec + 700; 
+                    txt_tot.Text = tot.ToString();
+                }
+            }
+            else if(cmb_type.SelectedIndex == 1)
+            {
+
+            }
+        }
+    }
+}
